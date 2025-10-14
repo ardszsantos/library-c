0f25066d30e03dcf4436e5cc0d28f2a9be644138
Alexandre Rodrigues <lokgamer01@gmail.com>
2025-10-14 13:58:59 -0300

    improving the main-menu

--- src/main.c
+++ src/main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "./structs/usuarios.h"
+#include "./structs/usuarios/usuarios.h"
+#include "./structs/livros/livros.h"
 #include "utils/user_functions.h"
+#include "utils/livro_functions.h"
 
 
 #define MAX_USUARIOS 100
-#define ARQUIVO_USUARIOS "usuarios.txt"
-
-Usuario* currentUser = NULL;
+#define MAX_LIVROS 500
 
 void mostrarBemVindo() {
     printf("====================================\n");
@@ -17,47 +17,92 @@ void mostrarBemVindo() {
     printf("====================================\n\n");
 }
 
+void menuPrincipal() {
+    printf("\n=== MENU PRINCIPAL ===\n");
+    printf("1 - Gerenciar Usuários\n");
+    printf("2 - Gerenciar Livros\n");
+    printf("3 - Empréstimos\n");
+    printf("4 - Relatórios\n");
+    printf("0 - Sair\n");
+    printf("Escolha uma opção: ");
+}
+
+void menuUsuarios() {
+    printf("\n=== GERENCIAR USUÁRIOS ===\n");
+    printf("1 - Cadastrar usuário\n");
+    printf("2 - Listar usuários\n");
+    printf("3 - Pesquisar usuário\n");
+    printf("0 - Voltar\n");
+    printf("Escolha uma opção: ");
+}
+
+void menuLivros() {
+    printf("\n=== GERENCIAR LIVROS ===\n");
+    printf("1 - Cadastrar livro\n");
+    printf("2 - Listar livros\n");
+    printf("3 - Pesquisar livro\n");
+    printf("0 - Voltar\n");
+    printf("Escolha uma opção: ");
+}
+
 int main() {
-    int opcao;
+    int opcao, subOpcao;
     mostrarBemVindo();
 
     do {
-        if (currentUser == NULL) {
-            // MENU PRÉ-LOGIN
-            printf("1 - Cadastrar novo usuário\n");
-            printf("2 - Fazer login\n");
-        } else {
-            // MENU PÓS-LOGIN
-            printf("1 - Listar usuários\n");
-            printf("2 - Outros métodos\n");
-            printf("3 - Logout\n");
-        }
-        printf("0 - Sair\n");
-        printf("Escolha uma opção: ");
+        menuPrincipal();
         scanf("%d", &opcao);
-        getchar(); // limpa \n
+        getchar();
 
-        if (currentUser == NULL) {
-            switch(opcao) {
-                case 1: cadastrarUsuario(); break;
-                case 2: logarUsuario(); break;
-                case 0: printf("Saindo...\n"); break;
-                default: printf("Opção inválida!\n");
-            }
-        } else {
-            switch(opcao) {
-                case 1: listarUsuarios(); break;
-                case 2: printf("WIP...\n"); break;
-                case 3:
-                    logout();
-                    break;
+        switch(opcao) {
+            case 1: // Usuários
+                do {
+                    menuUsuarios();
+                    scanf("%d", &subOpcao);
+                    getchar();
+                    
+                    switch(subOpcao) {
+                        case 1: cadastrarUsuario(); break;
+                        case 2: listarUsuarios(); break;
+                        case 3: printf("WIP - Pesquisar usuário\n"); break;
+                        case 0: break;
+                        default: printf("Opção inválida!\n");
+                    }
+                } while (subOpcao != 0);
+                break;
 
-                case 0: printf("Saindo...\n"); break;
-                default: printf("Opção inválida!\n");
-            }
+            case 2: // Livros
+                do {
+                    menuLivros();
+                    scanf("%d", &subOpcao);
+                    getchar();
+                    
+                    switch(subOpcao) {
+                        case 1: cadastrarLivro(); break;
+                        case 2: listarLivros(); break;
+                        case 3: pesquisarLivro(); break;
+                        case 0: break;
+                        default: printf("Opção inválida!\n");
+                    }
+                } while (subOpcao != 0);
+                break;
+
+            case 3: // Empréstimos
+                printf("WIP - Empréstimos\n");
+                break;
+
+            case 4: // Relatórios
+                printf("WIP - Relatórios\n");
+                break;
+
+            case 0:
+                printf("Encerrando sistema...\n");
+                break;
+
+            default:
+                printf("Opção inválida!\n");
         }
     } while (opcao != 0);
-
 
     return 0;
 }
